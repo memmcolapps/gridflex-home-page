c906b4ca72e6c1984f554b73b7c97b977569a1c2
macbook <macbook@Kitans-Pro.local>
2025-08-29 17:10:10 +0100

    Add: contact page

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         { name: "HES (Head-end System)", route: "/coreProducts/#Hes_Headend" },
       ],
     },
-    { name: "CONTACT US", route: "/(contact" },
+    { name: "CONTACT US", route: "/contactus" },
   ];
 
   useEffect(() => {
