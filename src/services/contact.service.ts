bec3904b92c8e023878f91e5160afbd627597257
macbook <macbook@Kitans-Pro.local>
2025-09-23 09:11:18 +0100

    Fix: failed build

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+export interface ContactResponse{
+    responsecode: string;
+    responsedesc: string;
+    responsedata: ContactMessage;
+}
+
 export interface ContactMessage {
   organizationName: string;
   organizationSize: string;
@@ -10,16 +16,18 @@ export interface ContactMessage {
   message: string;
 }
 
-export const createMessage = async (data: ContactMessage) => {
-  const response = await axios.post(
-    `${BASE_URL}/portal/onboard/v1/api/gfPortal/service/message/create`,
-    data,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  return response.data;
-};
+export const createMessage = async (
+    data: ContactMessage
+  ): Promise<ContactResponse> => {
+    const response = await axios.post<ContactResponse>(
+      `${BASE_URL}/portal/onboard/v1/api/gfPortal/service/message/create`,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  
+    return response.data;
+  };
