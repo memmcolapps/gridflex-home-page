a2ce6cea26621f8b331b627b67ef1174da4bb07a
macbook <macbook@Kitans-Pro.local>
2026-02-12 11:17:08 +0100

    fix

--- src/hooks/use-contact.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { createMessage, type ContactMessage } from "@/services/contact.service";
-
-export const useCreateMessage = () => {
-  return useMutation({
-    mutationFn: (data: ContactMessage) => createMessage(data),
-  });
-};
